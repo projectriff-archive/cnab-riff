4660056158a4404930867155c17158ccbc26c98a
Swapnil Bawaskar <sbawaskar@pivotal.io>
2019-04-25 14:17:38 -0700

    publish snapshots of master

--- ci/publish.sh
+++ ci/publish.sh
@@ -4,14 +4,19 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-version=`cat VERSION`
+if [ "$#" -ne 1 ]; then
+  version=`cat VERSION`
+  bucket=gs://projectriff/riff-cnab/releases
+else
+  version="`cat VERSION`-${BUILD_NUMBER}"
+  bucket=gs://projectriff/riff-cnab/snapshots
+fi
+
 
 duffle export riff -t
 tar -xvzf riff-*.tgz
 mv bundle.* riff-bundle-${version}.json
 
 gcloud auth activate-service-account --key-file <(echo $GCLOUD_CLIENT_SECRET | base64 --decode)
-
-bucket=gs://projectriff/riff-cnab/releases
 
 gsutil cp -a public-read -n riff-bundle-${version}.json ${bucket}/
