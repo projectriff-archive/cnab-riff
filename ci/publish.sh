77363ab2343d48a0fd4c6b27f02f58a5f34fa2ca
Swapnil Bawaskar <sbawaskar@pivotal.io>
2019-04-25 11:33:09 -0700

    remove debug

--- ci/publish.sh
+++ ci/publish.sh
@@ -7,13 +7,10 @@ set -o pipefail
 version=`cat VERSION`
 
 duffle export riff -t
-ls -hal
 tar -xvzf riff-*.tgz
 mv bundle.* riff-bundle-${version}.json
 
-echo "foo is: $FOO"
-
-gcloud auth activate-service-account --key-file <(echo $(GcloudClientSecret) | base64 --decode)
+gcloud auth activate-service-account --key-file <(echo $GCLOUD_CLIENT_SECRET | base64 --decode)
 
 bucket=gs://projectriff/riff-cnab/releases
 
