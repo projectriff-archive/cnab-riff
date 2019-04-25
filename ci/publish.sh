a87ad80a6c3d3e4f9aded474d66f64459858565a
Swapnil Bawaskar <sbawaskar@pivotal.io>
2019-04-25 02:38:32 -0700

    try without env var

--- ci/publish.sh
+++ ci/publish.sh
@@ -13,7 +13,7 @@ mv bundle.* riff-bundle-${version}.json
 
 echo "foo is: $FOO"
 
-gcloud auth activate-service-account --key-file <(echo $GCLOUD_CLIENT_SECRET | base64 --decode)
+gcloud auth activate-service-account --key-file <(echo $(GcloudClientSecret) | base64 --decode)
 
 bucket=gs://projectriff/riff-cnab/releases
 
