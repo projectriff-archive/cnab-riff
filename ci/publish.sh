b30a5b1e1e952a3d78319258fa293efb7a404ca7
Scott Andrews <sandrews@pivotal.io>
2019-06-13 13:37:25 -0400

    Use gitsha rather than build number
    Build numbers are not deterministic and are difficult to reverse to
    actual code. Refspecs are unique, deterministic and easy to reverse.
    
    Streamline publishing to a single step. Releases are determined by the
    version. Versions that end with -snapshot are not releases.

--- ci/publish.sh
+++ ci/publish.sh
@@ -4,27 +4,16 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-if [ "$#" -ne 1 ]; then
-  echo "expected to receive one of (stage/snapshot/release) as argument"
-  exit 1
+version=`cat VERSION`
+commit=$(git rev-parse HEAD)
+bucket=gs://projectriff/riff-cnab
+
+staged_bundle="${bucket}/build/${build_file}/riff-bundle-${version}-${commit}.json"
+
+if echo "${version}" | grep -iq -e '-snapshot$'; then
+  # release if the version is not a '-snapshot'
+  gsutil cp -a public-read -n ${staged_bundle} ${bucket}/releases/riff-bundle-${version}.json
 fi
-
-build_bucket=gs://projectriff/riff-cnab/builds
-build_file="riff-bundle-`cat VERSION`-${BUILD_NUMBER}.json"
-
-if [ "$1" == "snapshot" ]; then
-  version="`cat VERSION`-${BUILD_NUMBER}"
-  bucket=gs://projectriff/riff-cnab/snapshots
-elif [ "$1" == "release" ]; then
-  version=`cat VERSION`
-  bucket=gs://projectriff/riff-cnab/releases
-else
-  echo "unknown publish argument"
-  exit 1
-fi
-
-dest_file="riff-bundle-${version}.json"
-
-gsutil cp -a public-read -n ${build_bucket}/${build_file} ${bucket}/${dest_file}
-gsutil cp -a public-read ${build_bucket}/${build_file} gs://projectriff/riff-cnab/snapshots/riff-bundle-`cat VERSION`.json
-gsutil cp -a public-read ${build_bucket}/${build_file} gs://projectriff/riff-cnab/snapshots/riff-bundle-latest.json
+gsutil cp -a public-read -n ${staged_bundle} ${bucket}/snapshots/riff-bundle-${version}-${commit}.json
+gsutil cp -a public-read    ${staged_bundle} ${bucket}/snapshots/riff-bundle-${version}.json
+gsutil cp -a public-read    ${staged_bundle} ${bucket}/snapshots/riff-bundle-latest.json
--- ci/stage-bundle.sh
+++ ci/stage-bundle.sh
@@ -4,17 +4,17 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-version="`cat VERSION`-${BUILD_NUMBER}"
-bucket=gs://projectriff/riff-cnab/builds
+version=`cat VERSION`
+commit=$(git rev-parse HEAD)
+bucket=gs://projectriff/riff-cnab
 
 duffle init
 duffle build .
 duffle export riff -t
 tar -xvzf riff-*.tgz
-mv bundle.* riff-bundle-${version}.json
 
 echo "$DOCKER_PASSWORD" | docker login --username $DOCKER_USERNAME --password-stdin
 gcloud auth activate-service-account --key-file <(echo $GCLOUD_CLIENT_SECRET | base64 --decode)
 
 docker push `duffle show riff | jq -r .invocationImages[0].image`
-gsutil cp -a public-read -n riff-bundle-${version}.json ${bucket}/
+gsutil cp -a public-read -n bundle.* ${bucket}/builds/riff-bundle-${version}-${commit}.json
